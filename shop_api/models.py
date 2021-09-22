c4af7260ef9ceb640656051929ca4f569fa2a2a8
Мартин Иден <idenmart07@gmail.com>
2021-09-22 19:59:12 +0600

    initial commit

---
+++ shop_api/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=3, decimal_places=2)
+    description = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+
+class Image(models.Model):
+    photo = models.ImageField(upload_to='images')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")
+    
+
